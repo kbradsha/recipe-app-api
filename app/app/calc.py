9b1cd7d913c8df03615e49c09bad6ba24197515e
bradshak@us.ibm.com <kbradsha@li-3e8be281-54c0-11cb-b3a7-fe4e016e0a43.ibm.com>
2022-07-21 15:54:19 -0500

    simple tests

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,11 +2,12 @@
 calculator functions
 """
 
+
 def add(x, y):
     """Add x & y, then return results"""
     return x + y
 
+
 def subtract(x, y):
     """Subtract x from y and return result"""
     return y - x
-
